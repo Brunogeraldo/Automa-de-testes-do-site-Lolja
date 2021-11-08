e8f51a8cba88b3dc33654b74c1a6e4826e6fa209
Brunogeraldo <brunogeraldo2011@gmail.com>
2021-11-08 18:19:28 -0300

    first commit

--- web/features/step_definitions/Sacola.rb
+++ web/features/step_definitions/Sacola.rb
@@ -1,18 +1,15 @@
 Quando("seleciono o primeiro produto da lista para a sacola") do
   first("[data-product-type='list']").click
 
-  sleep 2
-
-  find("[data-value='Preto']").click
-  find("[data-value='P']").click
-  find("[data-value='UNISSEX']").click
+  first("[data-value='P']").click
   first("[type='tel']").set "87000"
   first("[aria-label='Calcular frete']").click
-
+  sleep 2
   find("[value='Comprar']").click
 end
 
 Então("eu entro na página da sacola") do
   find("[data-toggle='#modal-cart']").click
-  expect(page).to have_css("[class='modal-body']")
+  sleep 1
+  expect(page).to have_css("[name='go_to_checkout']", visible: false)
 end
